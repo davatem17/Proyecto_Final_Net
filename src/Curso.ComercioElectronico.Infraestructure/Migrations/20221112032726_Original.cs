26ae6c740b4dd13d0481597fc01074dbf8b90cf2
davatem17 <perezd625@gmail.com>
2022-11-11 22:30:28 -0500

    Original

---
+++ src/Curso.ComercioElectronico.Infraestructure/Migrations/20221112032726_Original.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Curso.ComercioElectronico.Infraestructure.Migrations
+{
+    public partial class Original : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libros");
+
+            migrationBuilder.DropTable(
+                name: "Autors");
+
+            migrationBuilder.DropTable(
+                name: "Editorials");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Domicilio = table.Column<string>(type: "TEXT", nullable: false),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Editorials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    Ubicacion = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Editorials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AutorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EditorialId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FechaPublicacion = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libros_Autors_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Libros_Editorials_EditorialId",
+                        column: x => x.EditorialId,
+                        principalTable: "Editorials",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_AutorId",
+                table: "Libros",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_EditorialId",
+                table: "Libros",
+                column: "EditorialId");
+        }
+    }
+}
